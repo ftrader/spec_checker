6e60fb43ea94d0b7322bf52a01f4811e24c63747
ftrader <freetrader@tuta.io>
2017-01-13 18:01:10 +0100

    factor out Requirement and DesignElement classes into modules

--- spec_checker/runtime.py
+++ spec_checker/runtime.py
@@ -6,73 +6,7 @@
 import sys
 import re
 
-from spec_checker import util
-
-
-class Requirement(object):
-    ''' requirement class modeling a textual requirement. Not everything
-    is captured, currently only the name and traceability. '''
-
-    def __init__(self, name):
-        self.name = name
-        self.upstream_reqs = []
-        self.downstream_reqs = []
-        self.raw_trace_reqs = []
-        self.linked_design_elems = []
-
-    def add_raw_trace_req(self, input_str_or_list):
-        ''' add a raw trace requirement - raw means we do not yet know
-        if it is up or downstream '''
-        if type(input_str_or_list) == list:
-            for derived_req in input_str_or_list:
-                self.raw_trace_reqs.append(derived_req)
-        elif type(input_str_or_list) == str:
-            self.raw_trace_reqs.append(input_str_or_list)
-        else:
-            print("Unrecognized type - not list or string: %s" %
-                  input_str_or_list)
-            sys.exit(1)
-
-    def add_design_elem(self, input_str):
-        self.linked_design_elems.append(input_str)
-
-    def set_upstream_req(self, upstream_req):
-        ''' add to upstream (parent) requirements '''
-        self.upstream_reqs.append(upstream_req)
-
-    def are_ups_and_downs_consistent_with_raw_reqs(self):
-        if self.raw_trace_reqs:
-            # up and down must also be empty
-            return (not self.upstream_reqs and not self.downstream_reqs)
-        for rr in self.raw_trace_reqs:
-            if rr not in self.upstream_reqs and rr not in self.downstream_reqs:
-                return False
-        return True
-
-
-class DesignElement(object):
-    ''' class modeling a textual design element. Not everything
-    is captured, currently only the name and specified traceability to
-    SW reqs. '''
-
-    def __init__(self, name):
-        self.name = name
-        self.linked_reqs = []
-
-    def is_linked_to_req(self, req_name):
-        return req_name in self.linked_reqs
-
-    def add_reqs(self, input_str_or_list):
-        ''' add design element(s) '''
-        if type(input_str_or_list) == list:
-            for req in input_str_or_list:
-                self.linked_reqs.append(req)
-        elif type(input_str_or_list) == str:
-            self.linked_reqs.append(input_str_or_list)
-        else:
-            print("Unrecognized type - not list or string: %s" %
-                  input_str_or_list)
-            sys.exit(1)
+from spec_checker import util, requirement, designelement
 
 
 class TraceabilityChecker(object):
@@ -440,7 +374,7 @@ class TraceabilityChecker(object):
                         sys.exit(1)
 
                     if not self.find_requirement(last_req):
-                        last_req_obj = Requirement(last_req)
+                        last_req_obj = requirement.Requirement(last_req)
                         self.requirements.add(last_req_obj)
                         traceability = []
                     else:
@@ -561,7 +495,7 @@ class TraceabilityChecker(object):
                 des_elem = match_des_elem_intro_pat.group(1)
                 # design element reference found
                 if not self.find_design_elem(des_elem):
-                    des_elem_obj = DesignElement(des_elem)
+                    des_elem_obj = designelement.DesignElement(des_elem)
                     self.design_elements.add(des_elem_obj)
                 else:
                     print("Error in design file: duplicate design element "
